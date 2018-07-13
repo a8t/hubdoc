7aebad268330e4a4c3290705cb90786f432aefb6
Andy Tran <andy8tran@gmail.com>
2018-07-12 21:05:14 -0400

    Add comments for main scraper

--- main_scraper.js
+++ main_scraper.js
@@ -8,14 +8,19 @@ const puppeteer = require('puppeteer');
   await page.goto('https://datatables.net');
 
   const dataOutputObject = await page.evaluate(() => {
+    // Helper function for converting from nodelist to ES array so we can map
     const toArr = nodeList => Array.prototype.slice.call(nodeList);
 
+    // Grab the table rows from the webpage
     const rows = document.querySelectorAll('tr[role=row]');
     const rowsArr = toArr(rows);
 
+    // Turn the first row into an array of strings, to be used as headers
     const headerRow = rowsArr[0].children;
     const headerLabels = toArr(headerRow).map(each => each.innerText);
 
+    // Apply transforms to the rest of the data to get each cell in
+    // each row and zip the cell to the appropriate header
     const bodyRows = rowsArr.slice(1);
     const bodyRowInnertexts = bodyRows.map(toChildrenInnertext);
     const bodyRowObjects = bodyRowInnertexts.map(toObjectWithHeadingsAsKey);
@@ -40,6 +45,9 @@ const puppeteer = require('puppeteer');
     return bodyRowObjects;
   });
 
+  // Stringify the object and output it to a file.
+  // Could hand-roll the CSV stringify
+  // but that doesn't seem like how I'd do it in production.
   csvStringify(dataOutputObject, (err, output) => {
     fs.writeFileSync('./main.csv', output);
   });
